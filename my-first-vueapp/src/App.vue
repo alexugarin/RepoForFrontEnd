9fce8c7c14c8b5365d2ccaafd38bbd50f3e946a4
Alexey Ugarin <alexugarin@gmail.com>
2023-12-08 13:50:53 +0400

    task 9 create vue app

---
+++ my-first-vueapp/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <post-form
+        @createPost="createPost"
+    />
+    <post-list :posts="posts"/>
+    
+</template>
+
+<script>
+import PostForm from "@/components/PostForm.vue"
+import PostList from "@/components/PostList.vue"
+export default {
+    components: {
+        PostForm, PostList
+    },
+    data() {
+        return {
+            posts: [
+                {id: 1, title: 'JavaScript', body: 'Описание поста JS'},
+                {id: 2, title: 'JavaScript 2', body: 'Описание поста JS 2 '},
+                {id: 3, title: 'JavaScript 3', body: 'Описание поста JS 3'},
+            ]
+        }
+    },
+    methods: {
+        createPost(post, second) {
+            this.posts.push(post);
+        }
+    }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    
+}
+
+
+</style>
---
+++ my-first-vueapp/src/components/PostForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <form @submit.prevent>
+        <input 
+            v-model="post.title"
+            class="input" 
+            type="text" 
+            placeholder="Название поста"
+            >
+        <input 
+            v-model="post.body"
+            class="input" 
+            type="text" 
+            placeholder="Описание поста"
+            >
+        <button 
+           @click="createPost"
+        >Добавить пост</button>
+    </form>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return {
+            post: {
+                title: '',
+                body: ''
+            }
+        }
+    },
+    methods: {
+        createPost() {
+            this.post.id = Date.now();
+            this.$emit('createPost', this.post, 'second param')
+            this.post ={
+                title: '',
+                body: ''
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
